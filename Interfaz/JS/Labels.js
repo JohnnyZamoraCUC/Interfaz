e3b6cff5207c31fd5f7fc9b6799839323c360664
Johhny Zamora Guerrero <johnny.zamora.guerrero@cuc.cr>
2024-03-26 12:50:11 -0600

    cambios generales en nav y js de labels

--- Interfaz/JS/Labels.js
+++ Interfaz/JS/Labels.js
@@ -1,7 +1,6 @@
-﻿
-//JS PARA MOSTRAR LABELS
-const apiUrl = 'https://tiusr26pl.cuc-carrera-ti.ac.cr/BackendST/api/Labels/ObtenerLabels';
+﻿const apiUrl = 'https://tiusr26pl.cuc-carrera-ti.ac.cr/BackendST/api/Labels/ObtenerLabels';
 
+// Función para buscar la descripción desde la API
 function buscarDescripcionDesdeApi(idElemento) {
     return fetch(apiUrl)
         .then(response => {
@@ -11,23 +10,28 @@ function buscarDescripcionDesdeApi(idElemento) {
             return response.json();
         })
         .then(data => {
+            // Busca el elemento en el JSON por el nombre (idElemento)
             const elemento = data.find(item => item.Nombre === idElemento);
+            // Retorna la descripción si se encontró, de lo contrario, retorna una cadena vacía
             return elemento ? elemento.Descripcion : '';
         })
         .catch(error => {
             console.error('Se produjo un error:', error);
-            return '';
+            return ''; // Retorna una cadena vacía en caso de error
         });
 }
 
-const elementos = document.querySelectorAll('[id]');
+// Obtiene todos los elementos del navbar por su ID
+const elementos = document.querySelectorAll('a[id^="lbl"], button[id^="btn"]');
+
+// Itera sobre cada elemento para actualizar su contenido
 elementos.forEach(elemento => {
+    // Obtiene la descripción desde la API y actualiza el texto del elemento
     buscarDescripcionDesdeApi(elemento.id)
         .then(descripcion => {
+            // Si la descripción está vacía, se mantiene el ID como texto
             elemento.textContent = descripcion || elemento.id;
         });
 });
-
-
 
 
