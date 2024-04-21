0c54588e409d3e11230cd95da9ebc4b2b38a9c4a
Johhny Zamora Guerrero <johnny.zamora.guerrero@cuc.cr>
2024-04-21 11:42:03 -0600

    FINALES

--- Interfaz/usr/common.js
+++ Interfaz/usr/common.js
@@ -1,9 +1,9 @@
 ﻿
 $(document).ready(function () {
     // Cargar el encabezado desde header.html y mostrarlo en el elemento <header>
-    $('#header').load('/usr/header.html');
+    $('#header').load('/Interfaz/usr/header.html');
 });
 $(document).ready(function () {
     // Cargar el encabezado desde header.html y mostrarlo en el elemento <header>
-    $('#footer').load('/usr/footer.html');
+    $('#footer').load('/Interfaz/usr/footer.html');
 });
